df16e7627e73f8358ea34d13a980d42413876f5a
PMTSantos <81629693+PMTSantos@users.noreply.github.com>
2022-12-21 16:54:27 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,3 @@
-/*
- * Copyright (c) 2022. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
- * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
- * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
- * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
- * Vestibulum commodo. Ut rhoncus gravida arcu.
- */
-
-//POST W}2GY;[0Cf0W]xO066L]VvyDx{Tvl5w4
-//GET  9kG]tXKr[Kpnw8R6g0mYne.e}FKCeF}z
-
 var express = require("express");
 const con = require("./handlers/mysql.js");
 var bodyParser = require("body-parser");
@@ -16,97 +5,163 @@ var app = express();
 const multer = require('multer');
 var upload = multer({ storage: multer.memoryStorage() });
 const session = require('express-session');
+const crypto = require('crypto');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
 
 app.use(session({
-  resave: false, // don't save session if unmodified
-  saveUninitialized: false, // don't create session until something stored
-  secret: 's%3Al3ozSdvQ83TtC5RvJ.CibaQoHtaY0H3QOB1kqR8H2A',
-  cookie: {
-    expires: 3600000
-  }
+    resave: false, 
+    saveUninitialized: false, 
+    secret: 's%3Al3ozSdvQ83TtC5RvJ.CibaQoHtaY0H3QOB1kqR8H2A',
+    cookie: {
+        expires: 3600000
+    }
 }));
 
 global.db = (sql, values) => {
-  return new Promise((resolve, reject) => {
-    con.query(sql, values, (err, result) => {
-      if (err) reject(err);
-      resolve(result);
+    return new Promise((resolve, reject) => {
+        con.query(sql, values, (err, result) => {
+            if (err) reject(err);
+            resolve(result);
+        });
     });
-  });
 }
 
 app.listen(3000, () => {
-  console.log("Server running on port 3000");
+    console.log("Server running on port 3000");
 });
 
-const isAuthenticated = (req, res, next) => {
-  if (req.session.user) {
-    next();
-  } else {
-    res.status(401).send({
-      message: "Não estás autenticado!"
-    });
-  }
+const verifyAuth = (req, res, next) => {
+    const { token } = req.body;
+
+    if (req.session.user.token == token) {
+        next();
+    } else {
+        res.status(401).send({
+            message: "Não estás autenticado! Ou o token enviado está incorreto"
+        });
+    }
 }
 
 app.get("/api/auth", async (req, res, next) => {
-    const { apiKey, apiSecret } = req.body;
+    const { apiKey } = req.body;
 
-    var sql = "SELECT * FROM users WHERE apiKey = ? AND apiSecret = ?";
-    let data = await global.db(sql, [apiKey, apiSecret]);
+    var sql = "SELECT * FROM apikeys WHERE apiKEY = ?";
+    let data = await global.db(sql, [apiKey]);
 
-    if (data.length > 0) {
-        req.session.user = data[0];
-        res.status(200).send({
-            message: "Autenticado com sucesso!",
-            apiSecret: ""
-        });
+    if (!data[0]) return res.status(401).send({ message: "APIKey incorreta!" });
+
+    const value = crypto
+        .randomBytes(10)
+        .toString('hex')
+
+    req.session.user = {
+        apiKey: apiKey,
+        token: value
     }
+
+    setTimeout(() => {
+        delete req.session.user;
+    }, 900000);//15 minutos
+
+    res.status(200).send({
+        token: value,
+        "message": "Autenticado com sucesso! Usa o token para fazer pedidos à API. O token irã expirar em 15 minutos."
+    })
+
 });
 
-app.get("/horario/:apiKey/:turma", async function (req, res) {
-  const { apiKey, turma } = req.params;
+app.get("/horario/turma", async (req, res) => {
+    const { turma } = req.body;
 
-  if (apiKey === "9kG]tXKr[Kpnw8R6g0mYne.e}FKCeF}z") {
+    var sql = "SELECT * FROM turmas WHERE turma = ?";
+    let data = await global.db(sql, [turma]);
 
-  const sql = "SELECT info FROM horario WHERE turma = ?";
-  let result = await global.db(sql, [turma]);
-    res.send(result[0]);
-    
-  } else {
-    res.status(403);
-    res.end();
-  }
+    if (!data[0]) return res.status(404).send({ message: "Turma não encontrada!" });
+
+    res.status(200).send(data);
 });
 
-app.post("/horario/:apiKey/:turma", upload.single('horario'), async function (req, res) {
-  /*let apiKey = req.params.apiKey;
-  let turma = req.params.turma;
-  let json = req.body;
- 
-  if (apiKey == "W}2GY;[0Cf0W]xO066L]VvyDx{Tvl5w4") {
- 
-    var sql = `INSERT INTO horario (turma, info, versao) VALUES ('${turma}', '${JSON.stringify(json)}', ${version})`;
-    con(sql, function (err, result) {
-      if (err) throw err;
-      res.status(200);
-      res.end();
-    });
-    
-  } else {
-    res.status(403);
-    res.end();
-  }*/
-  console.log(req.file)
-  console.log(JSON.parse(req.body.data))
+app.get("/horario/turma/:turma", async (req, res) => {
 
-  let upFile = req.file.buffer.toString('base64');
-  let data = JSON.parse(req.body.data)
+    const { turma } = req.params;
 
-  var sql = `INSERT INTO horario (turma, info, versao, img) VALUES (?, ?, 2, ?)`
-  await global.db(sql, [req.params.turma, data, upFile])
+    var sql = "SELECT * FROM horario WHERE turma = ? AND active = 1";
+    let data = await global.db(sql, [turma]);
+
+    if (!data[0]) return res.status(404).send({ message: "Turma não encontrada!" });
+
+    res.status(200).send(data);
+
+});
+
+app.get("/horario/prof", async (req, res) => {
+
+    var sql = "SELECT nome FROM profs";
+    let data = await global.db(sql);
+
+    if (!data[0]) return res.status(404).send({ message: "Não foram encontrados horários!" });
+
+    res.status(200).send(data);
+
+});
+
+app.get("/horario/prof/:prof", async (req, res) => {
+
+    const { prof } = req.params;
+
+    var sql = "SELECT info FROM horario WHERE active = 1";
+    let data = await global.db(sql, [prof]);
+
+    let weekday = new Date().toLocaleString('en-us', { weekday: 'long' })
+    //get the hours and minutes
+    let hours = new Date().getHours();
+    let minutes = new Date().getMinutes();
+
+    switch (weekday) {
+        case "Monday":
+            weekday = "Segunda";
+            break;
+        case "Tuesday":
+            weekday = "Terça";
+            break;
+        case "Wednesday":
+            weekday = "Quarta";
+            break;
+        case "Thursday":
+            weekday = "Quinta";
+            break;
+        case "Friday":
+            weekday = "Sexta";
+            break;
+        default:
+            weekday = 0;
+    }
+
+    if (weekday == 0) return res.status(404).send({ message: "Hoje não há aulas!" });
+
+    data.filter(turma => {
+        turma.info.horario.find(horario => {
+            if(horario.dia == weekday) {
+                for (let i = 0; i < horario.info.length; i++) {
+                    if (i == horario.info.length - 1) {
+                        if (hours >= horario.info[i].hora[0] && minutes >= horario.info[i].minuto[0]) {
+                            return res.status(200).send(horario.info[i]);
+                        }
+                    } else {
+                        if (hours >= horario.info[i].hora[0] && minutes >= horario.info[i].minuto[0] && hours < horario.info[i + 1].hora[0] && minutes < horario.info[i + 1].minuto[0]) {
+                            return res.status(200).send(horario.info[i]);
+                        }
+                    }
+                }
+            }
+        })
+    })
+
+    if (!data[0]) return res.status(404).send({ message: "Professor não encontrado!" });
+
+    res.status(200).send(data);
+
 });
